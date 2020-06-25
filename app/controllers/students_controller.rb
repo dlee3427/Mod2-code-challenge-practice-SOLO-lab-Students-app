ee28e82e24a38a9fb437a3481abaa34f478f2145
dlee3427 <david.lee.sl2634@yale.edu>
2020-06-25 11:57:41 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -3,5 +3,44 @@ class StudentsController < ApplicationController
   def index
     @students = Student.all
   end
+
+  def new 
+    @student = Student.new
+  end 
+
+  def create 
+    @student = Student.new(student_params)
+    if @student.valid? 
+      @student.save
+      redirect_to instructor_path(@student.instructor)
+    else
+      flash[:error] = @student.errors.full_messages
+      redirect_to "/students/new"
+    end
+  end 
+
+  def show 
+    @student = Student.find(params[:id])
+  end 
+
+  def edit 
+    @student = Student.find(params[:id])
+  end 
+
+  def update 
+    @student = Student.find(params[:id])
+    if @student.valid?
+      @student.update(student_params)
+      redirect_to student_path(@student)
+    else
+      render "edit"
+    end 
+  end 
+
+  private 
+
+  def student_params
+    params.require(:student).permit(:name, :major, :age, :instructor_id)
+  end 
   
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,6 @@
 class Student < ApplicationRecord
     belongs_to :instructor
+
+    validates :age, numericality: { only_integers: true, greater_than: 18 }
+    
 end
